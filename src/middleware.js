dd9c60ed2dcce47ea54ffe956c5523037f258146
Raphael Beer <raphael.beer@protonmail.com>
2016-12-06 18:37:15 +0100

    Change: Logging

--- src/middleware.js
+++ src/middleware.js
@@ -43,15 +43,15 @@ middleware.doBasicAuth = (req, res, next) => {
   let userData = basicAuth(req) || { name: null, pass: null };
   let name = userData.name;
   let pass = userData.pass;
-  logInfo(`username: ${name}`);
-  logInfo(`password supplied: [${!pass ? ' ' : 'X'}]`);
+  logSuccess(`username: ${name}`);
+  !pass ? logWarn('No password!') : logSuccess('password supplied');
   db.user.findOne({ username: name }).then((userDoc) => {
     if (!userData || !name || !pass) {
       throw new Error();
     }
     userDoc.validatePassword(pass).then(() => {
       req.tokenPayload = userDoc.getTokenData();
-      logSuccess('Credentials accepted');
+      logSuccess('Basic Auth credentials accepted');
       return next();
     }).catch((err) => {
       logError(err.message);
@@ -85,7 +85,11 @@ middleware.canAccess = (req, res, next, data) => {
   let selfTest = data.allowSelf === req.tokenPayload.username;
   let grantedBy = isAdmin ? 'isAdmin' : accessTest ? 'access' : selfTest ? 'self' : '';
 
-  return (isAdmin || accessTest || selfTest) ? next(grantedBy) : fail();
+  if (isAdmin || accessTest || selfTest) {
+    logSuccess(`Requested access level (${data.accessType}) was satisfied by: ${grantedBy}`);
+    return next(grantedBy);
+  }
+  return fail();
 };
 
 let failAuthRequest = (res) => {
