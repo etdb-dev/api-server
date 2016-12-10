714665dc5039b6cbe0130583b5eb139e0be33fd8
Raphael Beer <raphael.beer@protonmail.com>
2016-12-10 15:30:31 +0100

    Refactor: Introduce hierarchy to apiController
      - .apps for app routes, .api for APIs, so on...

---
+++ src/controller/api/apps.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const Promise = require('bluebird');
+const _ = require('lodash');
+
+const mw = require('../../middleware');
+const App = require('../../db/app');
+
+const appsController = {};
+
+appsController.addApp = (req, res, next) => {
+  mw.canAccess(req, res, () => {
+    let data = req.body;
+    let tokenData = req.tokenPayload;
+
+    validateInput(['name', 'publisher', 'store_url'], data)
+    .then(() => {
+      let app = new App({
+        name: data.name,
+        publisher: data.publisher,
+        store_url: data.store_url
+      });
+      app.save().then(() => {
+        let msg = `${data.name} has been createad`;
+        logSuccess(msg + ' by ' + tokenData.username);
+        res.status(201).json({ message: msg });
+      }).catch(() => res.status(409).json({
+        message: `${data.name} already exists`
+      }));
+    })
+    .catch((missing) => {
+      res.status(400).json({
+        message: 'Please provide values for all mandatory fields!',
+        missing: missing
+      });
+    });
+  }, { accessType: 'writeAPI' });
+};
+
+appsController.listApps = (req, res, next) => {
+  mw.canAccess(req, res, () => {
+    let findFilter = req.params.appId ? { name: req.params.appId } : {};
+    App.find(findFilter, '-_id -__v').then((appDocs) => res.json({
+      message: 'applist',
+      apps: appDocs
+    }));
+  }, { accessType: 'readAPI' });
+};
+
+appsController.updateApp = (req, res, next) => {
+  mw.canAccess(req, res, () => {
+    let appId = req.params.appId;
+    let clientDoc;
+    return App.findOne({ name: appId }).then((appDoc) => {
+      _.assign(appDoc, req.body);
+      clientDoc = _.omit(appDoc._doc, '_id', '__v');
+      return appDoc.save();
+    }).then(() => {
+      res.json({
+        message: req.params.appId + ' has been updated',
+        updated: clientDoc
+      });
+    });
+  }, { accessType: 'writeAPI' });
+};
+
+appsController.deleteApp = (req, res) => {
+  mw.canAccess(req, res, () => {
+    return App.findOneAndRemove({ name: req.params.appId }, '-_id -__v')
+      .then((removedDoc) => {
+        res.json({
+          message: removedDoc.name + ' has been deleted'
+        });
+      });
+  }, { accessType: 'writeAPI' });
+};
+
+let validateInput = (expected, input) => {
+  return new Promise((resolve, reject) => {
+    let missingFields = _.difference(expected, _.keys(input));
+    missingFields.length > 0 ? reject(missingFields) : resolve();
+  });
+};
+
+module.exports = appsController;
--- src/router/api.js
+++ src/router/api.js
@@ -6,11 +6,11 @@ const middleware = require.main.require('./src/middleware');
 
 apiRouter.use('/v1', middleware.validateToken);
 
-apiRouter.post('/v1/apps', apiController.addApp);
-apiRouter.get('/v1/apps', apiController.listApps);
+apiRouter.post('/v1/apps', apiController.apps.addApp);
+apiRouter.get('/v1/apps', apiController.apps.listApps);
 
-apiRouter.get('/v1/apps/:appId', apiController.listApps);
-apiRouter.put('/v1/apps/:appId', apiController.updateApp);
-apiRouter.delete('/v1/apps/:appId', apiController.deleteApp);
+apiRouter.get('/v1/apps/:appId', apiController.apps.listApps);
+apiRouter.put('/v1/apps/:appId', apiController.apps.updateApp);
+apiRouter.delete('/v1/apps/:appId', apiController.apps.deleteApp);
 
 module.exports = apiRouter;
