7366f29bf39e21563abcb336826d26b90dfcbb67
Raphael Beer <raphael.beer@protonmail.com>
2016-12-14 04:36:10 +0100

    Refactor: apps-routes into own file

--- src/router/api.js
+++ src/router/api.js
@@ -5,12 +5,6 @@ const apiController = require.main.require('./src/controller/api');
 const middleware = require.main.require('./src/middleware');
 
 apiRouter.use('/v1', middleware.validateToken);
-
-apiRouter.post('/v1/apps', apiController.apps.addApp);
-apiRouter.get('/v1/apps', apiController.apps.listApps);
-
-apiRouter.get('/v1/apps/:appId', apiController.apps.listApps);
-apiRouter.put('/v1/apps/:appId', apiController.apps.updateApp);
-apiRouter.delete('/v1/apps/:appId', apiController.apps.deleteApp);
+require('./api/apps')(apiRouter, apiController);
 
 module.exports = apiRouter;
