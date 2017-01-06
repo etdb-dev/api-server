61d974f698b45b9518052de04c5a0824c3ab326b
Raphael Beer <raphael.beer@protonmail.com>
2017-01-06 03:32:39 +0100

    Rework: should be fine :smirk:
      - docs/etdb/0.0.9/AccessRequest.html

--- src/access-request.cls.js
+++ src/access-request.cls.js
@@ -3,11 +3,13 @@
 const routeLevels = require('./route-access-levels.map.json');
 
 /**
+ * @version 0.1.0
  * @class AccessRequest Used to verify access rights on API requests.
  */
 class AccessRequest {
 
   /**
+   * @version 0.1.0
    * @typedef AccessLevels
    * @description No levels grant other levels implicitly, except isAdmin; i.e. a user with `writeAPI=true` can't automatically also read the API.
    * @memberOf AccessRequest
@@ -19,13 +21,27 @@ class AccessRequest {
    */
 
   /**
-   * Takes an object with request data and returns a new AccessRequest object.
-   * @param  {Object}             data
-   * @param  {Express.Request}    data.req         Express req of the handled request.
-   * @param  {Express.Response}   data.res         Express res of the handled request.
-   * @param  {String}             data.neededLevel AccessLevel the request needs granted.
-   * @param  {?ObjectId}          data.allowSelf   Mongo ObjectId, identifying user to allow editing/listing own data.
-   * @throws {TypeError}          If !data.req || !data.res || !data.neededLevel
+   * @version 0.1.0
+   * @typedef AllowSelf
+   * @description AllowSelf values of an AccessRequest instance.  
+   *              Exposed by {@link AccessRequest#allowSelf}
+   * @memberOf AccessRequest
+   * @static
+   * @property {Boolean} active  Whether this request's route
+   *                             offers access to `self` functions
+   * @property {String}  userId  **MongoDB ObjectId, identifying the user owning
+   *                             the request's JTW.**  
+   *                             Always an empty string (`''`), whenever .active
+   *                             is `false`.
+   */
+
+  /**
+   * @version 0.1.0
+   * @description
+   * Takes Express Request and Response objects and returns a new AccessRequest object for the request.
+   * @param  {Express.Request}    req  Express req of the handled request.
+   * @param  {Express.Response}   res  Express res of the handled request.
+   * @throws {TypeError}          If !req || !res
    * @return {AccessRequest}
    */
   constructor(req, res) {
@@ -35,54 +51,144 @@ class AccessRequest {
     }
 
     /**
-     * The data stored in the request's JSON Web Token.
-     * @type {TokenPayload}
-     */
-    this.tokenData = req.tokenPayload;
-    /**
-     * Access data of the user sending the request.
-     * @type {AccessLevels}
-     */
-    this.userAccess = this.tokenData.access;
-    /**
+     * @version 0.1.0
+     * @description
      * Express req object of the request.
+     * @name  req
+     * @instance
+     * @memberOf AccessRequest
      * @type {Express.Request}
      */
     this.req = req;
     /**
+     * @version 0.1.0
+     * @description
      * Express res object of the request.
+     * @name  res
+     * @instance
+     * @memberOf AccessRequest
      * @type {Express.Response}
      */
     this.res = res;
     /**
-     * AccessLevel needed to grant access to the requested API function.
-     * @type {String}
+     * @version 0.1.0
+     * @description
+     * The data stored in the request's JSON Web Token.
+     * @name  tokenData
+     * @instance
+     * @memberOf AccessRequest
+     * @type {TokenPayload}
      */
-    this._neededLevel = '';
+    this.tokenData = req.tokenPayload;
     /**
-     * MongoDB ObjectId, identifying a user. This can be used to allow users access to their own data, e.g. edit user information.
-     * @type {?String}
+     * @version 0.1.0
+     * @description
+     * MongoDB ObjectId of token owning user
+     * @name  userId
+     * @instance
+     * @memberOf AccessRequest
+     * @type {string}
      */
-    this._allowSelf = false;
+    this.userId = this.tokenData.userId;
+    /**
+     * @version 0.1.0
+     * @description
+     * Access data of the user sending the request.
+     * @name  userAccess
+     * @instance
+     * @memberOf AccessRequest
+     * @type {AccessLevels}
+     */
+    this.userAccess = this.tokenData.access;
+
+    this._routeAccess;
+    this._neededLevel = '';
+    this._allowSelf;
   }
 
+  /**
+   * @version 0.1.0
+   * @description
+   * **AccessLevel needed to grant access to the requested API function.**  
+   * Gets neededLevel from /src/route-access-levels.map.json.
+   * @name neededLevel
+   * @instance
+   * @memberOf AccessRequest
+   * @type {String}
+   */
   get neededLevel() {
-    if (this._neededLevel !== '') {
-      return this._neededLevel;
+    if (this._neededLevel === '') {
+      this._neededLevel = this.routeAccess[0] || 'none';
     }
-    let originalUrl = this.req.originalUrl;
-    let methods = routeLevels[originalUrl];
-    if (!methods) {
-      let lastSlashIdx = originalUrl.lastIndexOf('/') + 1;
-      if (lastSlashIdx < originalUrl.length) {
-        let routeUrl = originalUrl.slice(0, lastSlashIdx) + '*';
-        methods = routeLevels[routeUrl];
-      }
-    }
-    this._neededLevel = methods ? methods[this.req.method] || 'none' : 'none';
     return this._neededLevel;
   }
 
+  /**
+   * @version 0.1.0
+   * @description
+   * **Defines whether this request can edit user owned data or not.**
+   * @name allowSelf
+   * @instance
+   * @memberOf AccessRequest
+   * @type {AccessRequest.AllowSelf}
+   */
+  get allowSelf() {
+    if (!this._allowSelf) {
+      this._allowSelf = {
+        allow: this.routeAccess[1],
+        userId: this.routeAccess[1] ? this.userId : ''
+      };
+    }
+    return this._allowSelf;
+  }
+
+  /**
+   * @version 0.1.0
+   * @description
+   * Raw access information for the request's route
+   * @name  routeAccess
+   * @instance
+   * @memberOf AccessRequest
+   * @type {Array.<string, Boolean>}
+   */
+  get routeAccess() {
+    if (!this._routeAccess) {
+      let oUrl = this.req.originalUrl;
+      let reqMethod = this.req.method;
+      let accessDataDefault = [ 'none', false ];
+
+      let methods = routeLevels[oUrl] || routeLevels[wildcardRoute(oUrl)];
+      let accessData = methods ? methods[reqMethod] : accessDataDefault;
+
+      if (!(accessData instanceof Array)) {
+        accessData = accessDataDefault;
+      } else if (accessData.length === 1) {
+        accessData.push(false);
+      }
+      this._routeAccess = accessData;
+    }
+    return this._routeAccess;
+  }
+
 }
+
+/**
+ * @version 0.1.0
+ * @description
+ * Takes a url string and replaces item-urls (e.g. `/v0/apps/appIdHere039fa23sf...`)
+ * with wildcard-urls (e.g. `/v0/apps/*`), used in the [access-levels map](https://github.com/etdb-dev/api-server/blob/feature/AccessRequest/src/route-access-levels.map.json).
+ * @memberOf AccessRequest
+ * @inner
+ * @param  {string} routeUrl Url string, for example req.originalUrl of an AccessRequest.
+ * @return {string}          wildcarded url or empty string
+ */
+let wildcardRoute = (routeUrl) => {
+  // find last index of '/' in url;
+  // add 1, 'cause we need the '/' itself, too
+  let lastSlashIdx = routeUrl.lastIndexOf('/') + 1;
+  // if last '/' is not at the end, cut url after it and concat '*'
+  // otherwise return empty string
+  return lastSlashIdx < routeUrl.length ? routeUrl.slice(0, lastSlashIdx) + '*' : '';
+};
 
 module.exports = AccessRequest;
--- test/server/access-request.cls.js
+++ test/server/access-request.cls.js
@@ -5,45 +5,140 @@ chai.use(require('chai-http'));
 const expect = chai.expect;
 
 const AccessRequest = require('../../src/access-request.cls');
+const routeLevels = require('../../src/route-access-levels.map.json');
 
 module.exports = (accessRequestUser) => {
 
   describe('AccessRequest', function() {
-    const _data = {
-      req: {
-        tokenPayload: {
-          userId: accessRequestUser._id,
-          username: accessRequestUser.username,
-          password: accessRequestUser.password,
-          access: accessRequestUser.access
-        }
-      },
-      res: 'foo',
-      neededLevel: 'isAdmin',
-      allowSelf: accessRequestUser._id
+
+    // dummy response object
+    let testRes = {};
+
+    /**
+     * "/v0/spis": {
+     *   "GET": [ "readAPI" ],
+     *   // used for test
+     *   "POST": [ "writeAPI" ]
+     * }
+     */
+    let noSelfReq = {
+      originalUrl: '/v0/spis',
+      method: 'POST',
+      tokenPayload: {
+        userId: accessRequestUser._id,
+        username: accessRequestUser.username,
+        password: accessRequestUser.password,
+        access: accessRequestUser.access
+      }
     };
 
-    let testRequest = new AccessRequest(_data);
+    /**
+     * "/auth/users": {
+     *   "GET": [ "manageUsers", true ]
+     * }
+     */
+    let allowSelfReq = {
+      originalUrl: '/auth/users',
+      method: 'GET',
+      tokenPayload: {
+        userId: accessRequestUser._id,
+        username: accessRequestUser.username,
+        password: accessRequestUser.password,
+        access: accessRequestUser.access
+      }
+    };
 
-    it('should have property tokenData', () => {
-      expect(testRequest).to.have.property('tokenData', _data.req.tokenPayload);
+    /**
+      * "/auth/*": {
+      *   "PUT": [ "manageUsers", true ],
+      *   // used for test
+      *   "DELETE": [ "manageUsers", true ]
+      * }
+     */
+    let wildcardReq = {
+      originalUrl: '/auth/9c38a5a002739a48f427e3884',
+      method: 'DELETE',
+      tokenPayload: {
+        userId: accessRequestUser._id,
+        username: accessRequestUser.username,
+        password: accessRequestUser.password,
+        access: accessRequestUser.access
+      }
+    };
+
+    let invalidReq = {
+      originalUrl: '/very/wrong/path',
+      method: 'GET',
+      tokenPayload: {
+        userId: accessRequestUser._id,
+        username: accessRequestUser.username,
+        password: accessRequestUser.password,
+        access: accessRequestUser.access
+      }
+    };
+
+    let allowSelfRequest = new AccessRequest(allowSelfReq, testRes);
+    let noSelfRequest = new AccessRequest(noSelfReq, testRes);
+    let wildcardRequest = new AccessRequest(wildcardReq, testRes);
+    let invalidRequest = new AccessRequest(invalidReq, testRes);
+
+    it('should have property #tokenData', () => {
+      expect(allowSelfRequest).to.have.property('tokenData', allowSelfReq.tokenPayload);
     });
 
-    it('should have property userAccess', () => {
-      expect(testRequest).to.have.property('userAccess', accessRequestUser.access);
+    it('should have property #userAccess', () => {
+      expect(allowSelfRequest).to.have.property('userAccess', accessRequestUser.access);
     });
 
-    it('should have property res', () => {
-      expect(testRequest).to.have.property('res', 'foo');
+    it('should have property #res', () => {
+      expect(allowSelfRequest).to.have.property('res');
     });
 
-    it('should have property neededLevel', () => {
-      expect(testRequest).to.have.property('neededLevel', _data.neededLevel);
+    describe('#routeAccess', () => {
+      it('should return access values for a route', () => {
+        expect(allowSelfRequest.routeAccess).to
+        .eql(routeLevels[allowSelfReq.originalUrl][allowSelfReq.method]);
+
+        expect(noSelfRequest.routeAccess).to
+        .eql(routeLevels[noSelfReq.originalUrl][noSelfReq.method]);
+      });
+      it('should find wildcard- (/auth/*) for item routes (/auth/9c38a5a...) routes', () => {
+        expect(wildcardRequest.routeAccess).to
+        .eql(routeLevels['/auth/*'][wildcardReq.method]);
+      });
+      it('should be ok with invalid requests', () => {
+        expect(invalidRequest.routeAccess).to.eql([ 'none', false ]);
+      });
     });
 
-    it('should have property allowSelf', () => {
-      expect(testRequest).to.have.property('allowSelf', _data.allowSelf);
+    describe('#neededLevel', () => {
+      it('with value \'manageUsers\' on ' + allowSelfReq.originalUrl, () => {
+        expect(allowSelfRequest.neededLevel).to.be.a('string').and.equal('manageUsers');
+      });
+      it('with value \'writeAPI\' on ' + noSelfReq.originalUrl, () => {
+        expect(noSelfRequest.neededLevel).to.be.a('string').and.equal('writeAPI');
+      });
+      it('should be ok with invalid requests', () => {
+        expect(invalidRequest.neededLevel).to.eql('none');
+      });
     });
 
+    describe('#allowSelf', () => {
+      it('with allow=true, userId=accessRequestUser._id on ' + allowSelfReq.originalUrl, () => {
+        expect(allowSelfRequest.allowSelf).to.be.an('object').and.include.keys('allow', 'userId');
+        expect(allowSelfRequest.allowSelf.allow).to.be.true;
+        expect(allowSelfRequest.allowSelf.userId).to.equal(accessRequestUser._id);
+      });
+
+      it('with allow=false, userId=\'\' on ' + noSelfReq.originalUrl, () => {
+        expect(noSelfRequest.allowSelf).to.be.an('object').and.include.keys('allow', 'userId');
+        expect(noSelfRequest.allowSelf.allow).to.be.false;
+        expect(noSelfRequest.allowSelf.userId).to.equal('');
+      });
+
+      it('should be ok with invalid requests', () => {
+        expect(invalidRequest.allowSelf).to.eql({ allow: false, userId: '' });
+      });
+    });
   });
 };
