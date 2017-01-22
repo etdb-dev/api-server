0af709f40481030f411f69d38be08aa87df2b721
Raphael Beer <raphael.beer@protonmail.com>
2017-01-22 14:40:42 +0100

    Add: forever-monitor to manage processes, logs, etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,48 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var jshint = require('gulp-jshint');
-var exec = require('child_process').exec;
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const jshint = require('gulp-jshint');
+
+const forever = require('forever-monitor');
 
 gulp.task('default', function() {
-    return gutil.log('Help\n\nUse one of the following parameters:\n\n' +
-            'watch - run the jslinter, when code has changed\n' +
-            'run - just run the server\n' +
-            'dev - run the server with linter in background\n' +
-            'test - run server and tests\n');
+  return gutil.log('Help\n\nUse one of the following parameters:\n\n' +
+                   'watch - run the jslinter, when code has changed\n' +
+                   'run - just run the server\n' +
+                   'dev - run the server with linter in background\n' +
+                   'test - run server and tests\n');
 });
 
 gulp.task('jshint', function() {
-    return gulp.src('./*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'));
+  return gulp.src('./*.js')
+         .pipe(jshint())
+         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./*.js', ['jshint']);
+  gulp.watch('./*.js', ['jshint']);
 });
 
-gulp.task('dev', function() {
-   return gutil.log('Running in dev mode!'); 
+gulp.task('dev', function(done) {
+  let child = new (forever.Monitor)('etdb.js', require('./.forever/etdb-api-dev.json'));
+  child.start();
+  return gutil.log('Running in dev mode!');
 });
 
 gulp.task('deploy', function() {
-    // NOTE: run tests on server
-    // deploy on server
-})
+  // NOTE: run tests on server
+  // deploy on server
+});
 
 gulp.task('publish', function() {
-    // NOTE: publish on github
-})
+  // NOTE: publish on github
+});
 
 gulp.task('run', function() {
-    // NOTE: Use fork
-    // fork('./etdb.js')
-    exec('node ./etdb.js', function(err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    });
-    
-    return gutil.log('Running ETdb api server');
+  // NOTE: Use fork
+  // fork('./etdb.js')
+  return gutil.log('Running ETdb api server');
 });
 
 gulp.task('test', function() {
-    return gutil.log('Running tests');
+  return gutil.log('Running tests');
 });
